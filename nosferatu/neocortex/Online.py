998d7c82ad0c52be2c4bc75f70d18241c7c98fa0
ustyuzhaninky <ustyuzhanin.ky@gmail.com>
2020-05-27 19:49:04 +0500

    DBM neocortex first introduction

---
+++ nosferatu/neocortex/Online.py
@@ -0,0 +1,305 @@
+# coding=utf-8
+# Copyright 2020 Konstantin Ustyuzhanin.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# author: Konstantin Ustyuzhanin
+#
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import sys
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.framework import tensor_shape
+from tensorflow.python.keras.engine.input_spec import InputSpec
+from tensorflow.python.eager import context
+from tensorflow import nn
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras import constraints
+from tensorflow.python.keras import initializers
+from tensorflow.python.keras import regularizers
+from tensorflow.python.keras.engine.base_layer import Layer
+from tensorflow.python.keras.utils import tf_utils
+from tensorflow.python.util.tf_export import keras_export
+from tensorflow.python.keras.utils import tf_utils
+from tensorflow.python.util import nest
+from tensorflow.python.framework import auto_control_deps
+from tensorflow.compat.v1.keras.preprocessing.sequence import pad_sequences
+from tensorflow.python.training.tracking import base as trackable
+from tensorflow.python.training.tracking import data_structures
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import tensor_util
+from tensorflow.python.keras.engine import base_layer_utils
+from tensorflow.python.keras.engine import input_spec
+from tensorflow.python.autograph.impl import api as autograph
+from tensorflow.python.autograph.core import ag_ctx
+from tensorflow.python.framework import errors
+
+from tensorflow.keras import activations
+from tensorflow.keras import initializers
+from tensorflow.keras import constraints
+from tensorflow.keras import optimizers
+from tensorflow.keras import losses
+
+class OnlineBolzmannCell(Layer):
+    '''
+    A cell implementing an online bolzmann restricted machine layer in TensorFlow 2
+    '''
+
+    def __init__(self,
+                 units,
+                 learning_rate=0.01,
+                 online=True,
+                 n_passes=1,
+                 return_hidden=True,
+                 use_bias=True,
+                 visible_activation='sigmoid',
+                 hidden_activation='sigmoid',
+                 kernel_initializer='glorot_uniform',
+                 bias_initializer='glorot_uniform',
+                 kernel_regularizer='l2',
+                 bias_regularizer='l2',
+                 activity_regularizer='l2',
+                #  kernel_constraint=None,
+                kernel_constraint=constraints.MinMaxNorm(
+                     min_value=-1.0, max_value=1.0, rate=1.0, axis=-1
+                ),
+                #  bias_constraint=None,
+                bias_constraint=constraints.MinMaxNorm(
+                     min_value=-1.0, max_value=1.0, rate=1.0, axis=-1
+                ),
+                 optimizer='Adam',
+                 ** kwargs):
+        
+        if 'input_shape' not in kwargs and 'input_dim' in kwargs:
+            kwargs['input_shape'] = (kwargs.pop('input_dim'),)
+
+        super(OnlineBolzmannCell, self).__init__(
+            activity_regularizer=regularizers.get(activity_regularizer), **kwargs)
+
+        self.units = units
+        self.learning_rate = learning_rate
+        self.online = online
+        self.return_hidden = return_hidden
+        self.visible_activation = activations.get(visible_activation)
+        self.hidden_activation = activations.get(hidden_activation)
+        self.use_bias = use_bias
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+        self.kernel_constraint = constraints.get(kernel_constraint)
+        self.bias_constraint = constraints.get(bias_constraint)
+        self.optimizer = optimizers.get(optimizer)
+        self.optimizer.learning_rate = self.learning_rate
+        self.n_passes = n_passes
+
+        self.supports_masking = True
+        self.input_spec = InputSpec(min_ndim=2)
+    
+    def build(self, input_shape):
+        dtype = dtypes.as_dtype(self.dtype or K.floatx())
+        if not (dtype.is_floating or dtype.is_complex):
+            raise TypeError('Unable to build `OnlineBolzmannCell` layer with non-floating point '
+                            'dtype %s' % (dtype,))
+        input_shape = tensor_shape.TensorShape(input_shape)
+        if tensor_shape.dimension_value(input_shape[-1]) is None:
+            raise ValueError('The last dimension of the inputs to `OnlineBolzmannCell` '
+                            'should be defined. Found `None`.')
+
+        last_dim = tensor_shape.dimension_value(input_shape[-1])
+        batch_dim = tensor_shape.dimension_value(input_shape[0])
+        self.input_spec = InputSpec(min_ndim=2, axes={-1: last_dim})
+
+        self.kernel = self.add_weight(
+            name='w',
+            shape=(last_dim, self.units),
+            constraint=self.kernel_constraint,
+            initializer=self.kernel_initializer,
+            regularizer=self.kernel_regularizer,
+            dtype=self.dtype,
+            trainable=True)
+        self.m, self.r = None, None
+        
+        if self.use_bias:
+            self.bias_hidden = self.add_weight(
+                name='b_h',
+                shape=(self.units,),
+                constraint=self.bias_constraint,
+                initializer=self.bias_initializer,
+                regularizer=self.bias_regularizer,
+                dtype=self.dtype,
+                trainable=True)
+            self.bias_visible = self.add_weight(
+                name='b_v',
+                shape=(last_dim,),
+                constraint=self.bias_constraint,
+                initializer=self.bias_initializer,
+                regularizer=self.bias_regularizer,
+                dtype=self.dtype,
+                trainable=True)
+
+            self.m_h, self.m_v, self.r_h, self.r_v = None, None, None, None
+        else:
+            self.bias_hidden = None
+            self.bias_visible = None
+        self.built = True
+
+    def call(self, inputs):
+        rank = inputs.shape.rank
+        if rank is not None and rank > 2:
+            # Broadcasting is required for the inputs.
+            inputs = tf.cast(inputs, dtype=self._compute_dtype)
+
+        hidden_units = self.prop_up(inputs)
+        visible_units = self.prop_down(hidden_units)
+        randoms_uniform_values = tf.random.uniform(
+            (1, self.units))
+        sample_hidden_units = tf.cast(
+            randoms_uniform_values < hidden_units, dtype=tf.float32)
+        self.states = sample_hidden_units
+        
+        if self.return_hidden:
+            outputs = self.states
+        else:
+            outputs = visible_units
+        
+        if rank is not None and rank > 2:
+            shape = inputs.shape.as_list()
+            output_shape = shape[:-1] + [self.units]
+            outputs.set_shape(output_shape)
+
+        if self.online:
+            self.pcd_k_step(inputs)
+            # self.competitive_step(inputs)
+            
+
+        return outputs
+
+    def pcd_k_step(self, inputs):
+        hidden_units = self.prop_up(inputs)
+        visible_units = self.prop_down(hidden_units)
+        randoms_uniform_values = tf.random.uniform(
+            (1, self.units))
+        sample_hidden_units = tf.cast(
+            randoms_uniform_values < hidden_units, dtype=tf.float32)
+
+        # Positive gradient
+        # Outer product. N is the batch size length.
+        # From http://stackoverflow.com/questions/35213787/tensorflow-batch-outer-product
+        positive_grad = tf.matmul(
+            tf.transpose(sample_hidden_units), inputs,
+            a_is_sparse=K.is_sparse(sample_hidden_units), b_is_sparse=K.is_sparse(inputs))
+
+        # Negative gradient
+        # Gibbs sampling
+        sample_hidden_units_gibbs_step = sample_hidden_units
+        for t in range(self.n_passes):
+            compute_visible_units = self.prop_down(
+                sample_hidden_units_gibbs_step)
+            compute_hidden_units_gibbs_step = self.prop_up(
+                compute_visible_units)
+            random_uniform_values = tf.random.uniform(
+                (1, self.units))
+            sample_hidden_units_gibbs_step = tf.cast(
+                random_uniform_values < compute_hidden_units_gibbs_step, dtype=tf.float32)
+
+        negative_grad = tf.matmul(tf.transpose(sample_hidden_units_gibbs_step),
+                                    compute_visible_units,
+                                    a_is_sparse=K.is_sparse(
+                                        sample_hidden_units_gibbs_step),
+                                    b_is_sparse=K.is_sparse(compute_visible_units))
+
+        # tf.reduce_mean(positive_grad - negative_grad, 0)
+        grad_kernel = tf.transpose(
+            positive_grad - negative_grad)
+        if self.use_bias:
+            grad_visible = tf.reduce_mean(
+                inputs - compute_visible_units, 0)
+            grad_hidden = tf.reduce_mean(
+                sample_hidden_units - sample_hidden_units_gibbs_step, 0)
+            self.bias_visible.assign_add(-self.learning_rate*grad_visible)
+            self.bias_hidden.assign_add(-self.learning_rate*grad_hidden)
+        self.kernel.assign_add(self.learning_rate*grad_kernel)
+
+    # TODO: Try to implemment SOM learning
+    # def _gaussian_neighborhood(self, input_vector, sigma=0.2):
+    #     """ calculates gaussian distance """
+    #     return (1 / tf.sqrt(2*np.pi*sigma**2)) * tf.exp(-1*input_vector**2 / (2*sigma**2))
+
+    def competitive_step(self, inputs):
+        #computing closeness of 
+
+        hidden_units = self.prop_up(inputs)
+        visible_units = self.prop_down(hidden_units)
+        distances = inputs - visible_units
+        loss = distances.shape[-1] * distances**2
+        print((self._gaussian_neighborhood(distances)*loss).shape, tf.transpose(self.kernel).shape)
+        grad_kernel = tf.transpose(tf.matmul(tf.transpose(self.kernel), self._gaussian_neighborhood(distances)*loss))
+        
+        self.kernel.assign_add(-self.learning_rate*grad_kernel)
+        # if self.use_bias:
+            # grad_visible = tf.reduce_mean(
+                # inputs - compute_visible_units, 0)
+            # grad_hidden = tf.reduce_mean(
+                # sample_hidden_units - sample_hidden_units_gibbs_step, 0)
+            # self.bias_visible.assign_add(-self.learning_rate*grad_visible)
+            # self.bias_hidden.assign_add(-self.learning_rate*grad_hidden)
+    
+
+    def prop_up(self, v, q=1):
+        """ upwards mean-field propagation """
+        if K.is_sparse(v):
+            outputs = tf.matmul(v, self.kernel, a_is_sparse=True)
+        else:
+            outputs = tf.matmul(v, self.kernel)
+        if self.use_bias:
+            outputs = nn.bias_add(outputs, self.bias_hidden)
+        return self.hidden_activation(q*outputs)
+
+    def prop_down(self, h, q=1, sig=True):
+        """ downwards mean-field propagation """
+        if K.is_sparse(h):
+            outputs = tf.matmul(h, tf.transpose(self.kernel), a_is_sparse=True)
+        else:
+            outputs = tf.matmul(h, tf.transpose(self.kernel))
+        if self.use_bias:
+            outputs = nn.bias_add(outputs, self.bias_visible)
+        return self.visible_activation(q*outputs)
+
+    def get_config(self):
+        config = {
+            'units': self.units,
+            'learning_rate': self.learning_rate,
+            'online': self.online,
+            'n_passes': self.n_passes,
+            'return_hidden': self.return_hidden,
+            'visible_activation': activations.serialize(self.visible_activation),
+            'hidden_activation': activations.serialize(self.hidden_activation),
+            'use_bias': self.use_bias,
+            'kernel_initializer': initializers.serialize(self.kernel_initializer),
+            'bias_initializer': initializers.serialize(self.bias_initializer),
+            'kernel_regularizer': regularizers.serialize(self.kernel_regularizer),
+            'bias_regularizer': regularizers.serialize(self.bias_regularizer),
+            'activity_regularizer': regularizers.serialize(self.activity_regularizer),
+            'kernel_constraint': constraints.serialize(self.kernel_constraint),
+            'bias_constraint': constraints.serialize(self.bias_constraint),
+            'optimizer':  optimizers.serialize(self.optimizer)
+        }
+
+        base_config = super(OnlineBolzmannCell, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
---
+++ tests/test_Online.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import pickle
+import datetime
+import argparse
+import re
+import glob
+from nosferatu.neocortex.Online import OnlineBolzmannCell
+import tensorflow as tf
+import numpy as np
+import matplotlib.image as mpimg
+from skimage.transform import resize
+from tensorflow.keras import metrics
+import pandas as pd
+from sklearn.metrics import classification_report, accuracy_score, plot_confusion_matrix
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+import matplotlib.pyplot as plt
+from tensorflow.keras.metrics import categorical_accuracy
+
+tf.config.experimental_run_functions_eagerly(True)
+
+print("TensorFlow version: {}".format(tf.__version__))
+print("Eager execution: {}".format(tf.executing_eagerly()))
+
+tf.config.experimental_run_functions_eagerly(True)
+
+def trainRBM(data, learning_rate, k1, k2, epochs, batch_size, dims):
+    # import data
+    print("importing training data")
+    if data == "fashion_mnist":
+        fashion_mnist = tf.keras.datasets.fashion_mnist
+        (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+    elif data == "mnist":
+        mnist = tf.keras.datasets.mnist
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+        y_train = tf.keras.utils.to_categorical(y_train, 10)
+        y_test = tf.keras.utils.to_categorical(y_test, 10)
+    elif data == "faces":
+        x_train = [resize(mpimg.imread(file),(28,28)) for file in glob.glob("data/faces/*")]
+        x_train = np.asarray(x_train)
+        # make images sparse for easier distinctions
+        for img in x_train:
+            img[img < np.mean(img)+0.5*np.std(img)] = 0
+    else:
+        raise NameError("unknown data type: %s" % data)
+    if data == "mnist" or data == "fashion_mnist":
+        x_train = x_train/255.0
+        x_test = x_test/255.0
+        x_train = tf.cast(tf.reshape(x_train, shape=(x_train.shape[0], 784)), "float32")
+        x_test = tf.cast(tf.reshape(
+            x_test, shape=(x_test.shape[0], 784)), "float32")
+    elif data == "faces":
+        # auto conversion to probabilities in earlier step
+        x_train = tf.cast(tf.reshape(
+            x_train, shape=(x_train.shape[0], 784)), "float32")
+
+    # train_path = tf.keras.utils.get_file(
+    #     "iris_training.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv")
+    # test_path = tf.keras.utils.get_file(
+    #     "iris_test.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv")
+    # CSV_COLUMN_NAMES = ['SepalLength', 'SepalWidth',
+    #                     'PetalLength', 'PetalWidth', 'Species']
+    # # SPECIES = ['Setosa', 'Versicolor', 'Virginica']
+    # train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
+    # test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
+
+    # y_train, y_test = train.pop('Species').values, test.pop('Species').values
+    # x_train, x_test = train.values, test.values
+    # y_train = tf.keras.utils.to_categorical(y_train, 3)
+    # y_test = tf.keras.utils.to_categorical(y_test, 3)
+
+    feature_dim = x_train.shape[-1]
+    output_dim = y_train.shape[-1]
+
+    # x_train = x_train.reshape(x_train.shape[0], x_train.shape[-1])
+    # create log directory
+    # parse string input into integer list
+    inputs = tf.keras.Input(feature_dim)
+    outputs = OnlineBolzmannCell(50, online=True, return_hidden=True)(inputs)
+    outputs = tf.keras.layers.Dropout(0.2)(outputs)
+    # outputs = tf.keras.layers.Dense(100)(inputs)
+    outputs = tf.keras.layers.Dense(output_dim, activation='sigmoid')(outputs)
+    rbm = tf.keras.Model(inputs, outputs)
+    rbm.compile(loss='categorical_crossentropy',
+                optimizer="adam", metrics=["accuracy"])
+    print('Training...')
+    rbm.fit(x_train, y_train, batch_size=batch_size, epochs=1, validation_split=0.1)
+    for i in range(epochs):
+        pred = rbm.predict(x_train)
+        score = rbm.evaluate(x_train, y_train, verbose=0)
+        print(f'Epoch {i+1}, loss={score[0]} Accuracy={score[1]}')
+    score = rbm.evaluate(x_test, y_test, verbose=0)
+    print("Test loss:", score[0])
+    print("Test accuracy:", score[1])
+    print(10*'#')
+    score_train = classification_report(
+        y_train.astype(np.bool), rbm.predict(x_train).astype(np.bool))
+    score_test = classification_report(
+        y_test, rbm.predict(x_test).astype(np.bool))
+    acc_train = categorical_accuracy(
+        y_train, rbm.predict(x_train))
+    acc_test = categorical_accuracy(
+        y_test, rbm.predict(x_test))
+    print(f'Train score:\nAccuracy: {acc_train}\n{score_train}')
+    print(f'Test score:\nAccuracy: {acc_test}\n{score_test}')
+    print(10*'#')
+    # plot_confusion_matrix(KerasClassifier(rbm, batch_size=batch_size), x_train, y_train>0.51)
+    # plot_confusion_matrix(KerasClassifier(
+        # rbm, batch_size=batch_size), x_test, y_test > 0.51)
+    plt.show()
+
+def getCurrentTime():
+    return datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+
+####################################
+# main command call
+####################################
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data", type=str, default="mnist",
+            help="data source to train RBM, possibilities are 'mnist', 'fashion_mnist' and 'faces' <default: 'mnist'>")
+    parser.add_argument("--learning-rate", type=float, default=0.01,
+                        help="learning rate for stacked RBMs <default: 0.01>")
+    parser.add_argument("--k1", type=int, default=1,
+            help="number of Gibbs-sampling steps pre-PCD-k algorithm <default: 1>")
+    parser.add_argument("--k2", type=int, default=5,
+            help="number of Gibbs-sampling steps during PCD-k algorithm <default: 5>")
+    parser.add_argument("--epochs", type=int, default=1,
+            help="number of overall training data passes for each RBM <default: 1>")
+    parser.add_argument("--batch-size", type=int, default=32,
+            help="size of training data batches <default: 32>")
+    # requiredNamed = parser.add_argument_group('required named arguments')
+    # requiredNamed.add_argument('-d', '--dimensions', type=str, 
+                            #    help="consecutive enumeration of visible and hidden units separated by a comma character, eg. 784,500", 
+                            #    required=True)
+    args = parser.parse_args()
+    # train RBM based on parameters
+    trainRBM(args.data,args.learning_rate,args.k1,args.k2,args.epochs,args.batch_size,None)
