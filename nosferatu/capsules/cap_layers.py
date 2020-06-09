feeb173eb5e87a6f8f400a9280203a01cb9c1431
ustyuzhaninky <ustyuzhanin.ky@gmail.com>
2020-06-09 10:19:09 +0500

    capsule fix

--- nosferatu/capsules/cap_layers.py
+++ nosferatu/capsules/cap_layers.py
@@ -29,20 +29,54 @@ from tensorflow.keras import backend as K
 from tensorflow.keras.layers import Layer
 
 def squash(x, axis=-1):
+    '''Squashing function corresponding to Eq. 1
+    Args:
+        x: A tensor with shape [batch_size, 1, num_caps, vec_len, 1] or [batch_size, num_caps, vec_len, 1].
+    Returns:
+        A tensor with the same shape as vector but squashed in 'vec_len' dimension.
+    '''
     s_squared_norm = K.sum(K.square(x), axis, keepdims=True) + K.epsilon()
-    scale = K.sqrt(s_squared_norm)/ (0.5 + s_squared_norm)
+    scale = K.sqrt(s_squared_norm) / (0.5 + s_squared_norm)
     return scale * x
-
 
 #define our own softmax function instead of K.softmax
 def softmax(x, axis=-1):
     ex = K.exp(x - K.max(x, axis=axis, keepdims=True))
     return ex/K.sum(ex, axis=axis, keepdims=True)
 
+# define the margin loss like hinge loss
+def margin_loss(y_true, y_pred):
+    lamb, margin = 0.5, 0.1
+    return K.sum(y_true * K.square(K.relu(1 - margin - y_pred)) + lamb * (
+        1 - y_true) * K.square(K.relu(y_pred - margin)), axis=-1)
 
-#A Capsule Implement with Pure Keras
+
 class Capsule(Layer):
-    def __init__(self, num_capsule, dim_capsule, routings=3, share_weights=True, activation='squash', **kwargs):
+    """A Capsule Implement with Pure Keras
+    There are two vesions of Capsule.
+    One is like dense layer (for the fixed-shape input),
+    and the other is like timedistributed dense (for various length input).
+
+    The input shape of Capsule must be (batch_size,
+                                        input_num_capsule,
+                                        input_dim_capsule
+                                       )
+    and the output shape is (batch_size,
+                             num_capsule,
+                             dim_capsule
+                            )
+
+    Capsule Implement is from https://github.com/bojone/Capsule/
+    Capsule Paper: https://arxiv.org/abs/1710.09829
+    """
+
+    def __init__(self,
+                 num_capsule,
+                 dim_capsule,
+                 routings=3,
+                 share_weights=True,
+                 activation='squash',
+                 **kwargs):
         super(Capsule, self).__init__(**kwargs)
         self.num_capsule = num_capsule
         self.dim_capsule = dim_capsule
@@ -54,7 +88,79 @@ class Capsule(Layer):
             self.activation = activations.get(activation)
 
     def build(self, input_shape):
-        super(Capsule, self).build(input_shape)
+        input_dim_capsule = input_shape[-1]
+        if self.share_weights:
+            self.kernel = self.add_weight(
+                name='capsule_kernel',
+                shape=(1, input_dim_capsule,
+                       self.num_capsule * self.dim_capsule),
+                initializer='glorot_uniform',
+                trainable=True)
+        else:
+            input_num_capsule = input_shape[-2]
+            self.kernel = self.add_weight(
+                name='capsule_kernel',
+                shape=(input_num_capsule, input_dim_capsule,
+                       self.num_capsule * self.dim_capsule),
+                initializer='glorot_uniform',
+                trainable=True)
+
+    def call(self, inputs):
+        """Following the routing algorithm from Hinton's paper,
+        but replace b = b + <u,v> with b = <u,v>.
+
+        This change can improve the feature representation of Capsule.
+
+        However, you can replace
+            b = K.batch_dot(outputs, hat_inputs, [2, 3])
+        with
+            b += K.batch_dot(outputs, hat_inputs, [2, 3])
+        to realize a standard routing.
+        """
+
+        if self.share_weights:
+            hat_inputs = K.conv1d(inputs, self.kernel)
+        else:
+            hat_inputs = K.local_conv1d(inputs, self.kernel, [1], [1])
+
+        batch_size = K.shape(inputs)[0]
+        input_num_capsule = K.shape(inputs)[1]
+        hat_inputs = K.reshape(hat_inputs,
+                               (batch_size, input_num_capsule,
+                                self.num_capsule, self.dim_capsule))
+        hat_inputs = K.permute_dimensions(hat_inputs, (0, 2, 1, 3))
+        
+        b = K.zeros_like(hat_inputs[:, :, :, 0])
+        for i in range(self.routings):
+            c = softmax(b, 1)
+            # o = self.activation(K.batch_dot(c, hat_inputs, [2, 2])) # [2, 2]
+            o = tf.einsum('bin,binj->bij', c, hat_inputs)
+            # print(2, o.shape)
+            if i < self.routings - 1:
+                o = K.l2_normalize(o, -1)
+                # b = K.batch_dot(o, hat_inputs, [2, 3])
+                b = tf.einsum('bij,binj->bin', o, hat_inputs)
+
+        return o
+
+    def compute_output_shape(self, input_shape):
+        return (None, self.num_capsule, self.dim_capsule)
+
+#A Capsule Implement with Pure Keras
+class Capsule1D(Layer):
+    def __init__(self, num_capsule, dim_capsule, routings=3, share_weights=True, activation='squash', **kwargs):
+        super(Capsule1D, self).__init__(**kwargs)
+        self.num_capsule = num_capsule
+        self.dim_capsule = dim_capsule
+        self.routings = routings
+        self.share_weights = share_weights
+        if activation == 'squash':
+            self.activation = squash
+        else:
+            self.activation = activations.get(activation)
+
+    def build(self, input_shape):
+        super(Capsule1D, self).build(input_shape)
         input_dim_capsule = input_shape[-1]
         if self.share_weights:
             self.W = self.add_weight(name='capsule_kernel',
@@ -89,14 +195,10 @@ class Capsule(Layer):
             c = softmax(b, 1)
             # o = K.batch_dot(c, u_hat_vecs, [2, 2])
             o = tf.einsum('bin,binj->bij', c, u_hat_vecs)
-            if K.backend() == 'theano':
-                o = K.sum(o, axis=1)
             if i < self.routings - 1:
                 o = K.l2_normalize(o, -1)
                 # b = K.batch_dot(o, u_hat_vecs, [2, 3])
                 b = tf.einsum('bij,binj->bin', o, u_hat_vecs)
-                if K.backend() == 'theano':
-                    b = K.sum(b, axis=1)
 
         return self.activation(o)
 
